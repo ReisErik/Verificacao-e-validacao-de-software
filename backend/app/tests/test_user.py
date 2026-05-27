964b5eaf81494dd1d9ef833de9d548134483fd3a
ReisErik <erik.reis@outlook.com.br>
2026-05-27 11:48:13 -0300

    Criacao de testes unitarios para validacao de UserSchema

--- backend/app/tests/test_user.py
+++ backend/app/tests/test_user.py
@@ -4,6 +4,8 @@ from app.validators.validate_name import validate_name
 from app.validators.validate_password import validate_password
 from app.validators.validate_unique_name import validate_unique_name
 from fastapi import HTTPException
+from app.schemas.user_schema import UserCreateSchema
+from pydantic import ValidationError
 
 # Testes unitários para os validadores
 
@@ -55,6 +57,104 @@ def test_validate_unique_name_empty():
 def test_validate_unique_name_not_alphanumeric():
     with pytest.raises(HTTPException):
         validate_unique_name("joaosilva!@")
+
+# Teste unitarios para os Schemas
+
+def test_user_create_schema():
+    data = {
+        "first_name": "Joao",
+        "last_name": "Silva",
+        "unique_name": "joaosilva",
+        "email": "joao.silva@gmail.com",
+        "password": "senha123!",
+        "role": "user"
+    }
+    user_schema = UserCreateSchema(**data)
+    assert user_schema.model_dump() == data
+
+def test_user_create_schema_invalid_email():
+    data = {
+        "first_name": "Joao",
+        "last_name": "Silva",
+        "unique_name": "joaosilva",
+        "email": "joao.silva",
+        "password": "senha123!",
+        "role": "user"
+    }
+    with pytest.raises(ValidationError):
+        UserCreateSchema(**data)
+
+def test_user_create_schema_password_short():
+    data = {
+        "first_name": "Joao",
+        "last_name": "Silva",
+        "unique_name": "joaosilva",
+        "email": "joao.silva@gmail.com",
+        "password": "senha",
+        "role": "user"
+    }
+    with pytest.raises(ValidationError):
+        UserCreateSchema(**data)
+
+def test_user_create_schema_password_long():
+    data = {
+        "first_name": "Joao",
+        "last_name": "Silva",
+        "unique_name": "joaosilva",
+        "email": "joao.silva@gmail.com",
+        "password": "a"*21,
+        "role": "user"
+    }
+    with pytest.raises(ValidationError):
+        UserCreateSchema(**data)
+
+def test_user_create_schema_first_name_long():
+    data = {
+        "first_name": "a"*21,
+        "last_name": "Silva",
+        "unique_name": "joaosilva",
+        "email": "joao.silva@gmail.com",
+        "password": "senha123!",
+        "role": "user"
+    }
+    with pytest.raises(ValidationError):
+        UserCreateSchema(**data)
+
+def test_user_create_schema_last_name_long():
+    data = {
+        "first_name": "Joao",
+        "last_name": "a"*21,
+        "unique_name": "joaosilva",
+        "email": "joao.silva@gmail.com",
+        "password": "senha123!",
+        "role": "user"
+    }
+    with pytest.raises(ValidationError):
+        UserCreateSchema(**data)
+
+def test_user_create_schema_unique_name_long():
+    data = {
+        "first_name": "Joao",
+        "last_name": "Silva",
+        "unique_name": "a"*21,
+        "email": "joao.silva@gmail.com",
+        "password": "senha123!",
+        "role": "user"
+    }
+    with pytest.raises(ValidationError):
+        UserCreateSchema(**data)
+
+def test_user_create_schema_role_invalid():
+    data = {
+        "first_name": "Joao",
+        "last_name": "Silva",
+        "unique_name": "joaosilva",
+        "email": "joao.silva@gmail.com",
+        "password": "senha123!",
+        "role": "invalid_role"
+    }
+    with pytest.raises(ValidationError):
+        UserCreateSchema(**data)
 
 # Testes de integração, requisiçoes HTTP para as rotas de usuário
 
